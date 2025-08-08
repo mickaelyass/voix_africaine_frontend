06ec73d590d482db0e680975751ec9659d76e25a
bonheur <yassegoungbeseton@gmail.com>
2025-08-09 00:20:37 +0100

    4e commit

--- src/hooks/useAuth..ts
+++ src/hooks/useAuth..ts
@@ -1,12 +1,13 @@
+import axios from "axios";
 
-import apiClient from '../api/ apiClient';
 
+const API_URL = process.env.REACT_APP_API_URL;
 export const register = async (data: {
   email: string;
   password: string;
   full_name: string;
 }) => {
-  const response = await apiClient.post('/auth/register', data);
+  const response = await axios.post(`${API_URL}/auth/register`, data);
   return response.data;
 };
 
@@ -14,7 +15,7 @@ export const login = async (credentials: {
   username: string;
   password: string;
 }) => {
-  const response = await apiClient.post('/auth/token', 
+  const response = await axios.post(`${API_URL}/auth/token`, 
     `username=${credentials.username}&password=${credentials.password}`,
     {
       headers: {
@@ -27,7 +28,7 @@ export const login = async (credentials: {
 
 export const getCurrentUser = async () => {
   try {
-    const response = await apiClient.get('/auth/me');  // Notez /auth/me et non /auth/users/me
+    const response = await axios.get(`${API_URL}/auth/me`);  // Notez /auth/me et non /auth/users/me
     return response.data;
   } catch (error) {
     localStorage.removeItem('access_token');
